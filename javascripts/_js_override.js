77ab9e07a8ea61de801a1e5cfec70651ad45f972
clait <stefanomus@fastmail.com>
2021-10-03 00:34:59 +0100

    Purge & IP cost override

--- javascripts/_js_override.js
+++ javascripts/_js_override.js
@@ -1,11 +1,8 @@
 function override(tempClassID,tempRAID)	{
-/*	if (tempClassID==8||tempClassID==3||tempClassID==14||tempClassID==19||tempClassID==25||tempClassID==26||tempClassID==31)	{ */
-		/* Ignore Pain */
-/*		if (tempRAID==50)	{return 8;}	*/
-		/* purge */
-/*		if (tempRAID==59)	{return 4;}	*/
-/*	}*/
-
+	if (tempClassID==8||tempClassID==3||tempClassID==14||tempClassID==25||tempClassID==26||tempClassID==31||tempClassID==19)	{
+		if (tempRAID==50)	{return 8;}	/* Ignore Pain */
+		if (tempRAID==59)	{return 4;}	/* purge */
+	}
 	return 0;
 }
 
